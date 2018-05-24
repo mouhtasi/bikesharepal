5ed97824b3d2c8e79a70b09a4940547be7b27f1b
Imad Mouhtassem <imad@imadm.ca>
2018-05-24 19:52:35 -0400

    Add favicon and similar things

--- website/views.py
+++ website/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.staticfiles.templatetags.staticfiles import static
 
 
 def index(request):
     return render(request, 'website/index.html')
+
+
+def favicon(request, *args):
+    return redirect(static('favicon/' + request.build_absolute_uri().split('/')[-1]))
