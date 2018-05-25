15cd9f6cb5946073b962dffa18a265796b8e28a4
Imad Mouhtassem <imad@imadm.ca>
2018-05-24 21:36:20 -0400

    Change urls so that the station catalog page is the index page

--- bikesharestationcatalog/urls.py
+++ bikesharestationcatalog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.catalog_home, name='index'),  # since the catalog page is the index now
     path('', views.catalog_home, name='station_catalog_home'),
     path('station_details/<int:s_id>/', views.station_details, name='station_details'),
     path('about', views.about, name='about')
--- website/urls.py
+++ website/urls.py
@@ -2,9 +2,9 @@ from django.urls import path, include, re_path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', include('bikesharestationcatalog.urls')),
     re_path(r'^(android-chrome-192x192.png)|(android-chrome-512x512.png)|(apple-touch-icon.png)|(browserconfig.xml)|'
          +'(favicon.ico)|(favicon-16x16.png)|(favicon-32x32.png)|(mstile-150x150.png)|(safari-pinned-tab.svg)'
           '|(site.webmanifest)', views.favicon),
-    path('stations/', include('bikesharestationcatalog.urls')),
+    #path('stations/', include('bikesharestationcatalog.urls')),
 ]
