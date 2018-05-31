25be20385e52b93e285c2efeed706eba84a6a9af
Imad Mouhtassem <imad@imadm.ca>
2018-05-30 23:59:11 -0400

    Make service worker actually cache files

--- website/templates/service-worker.js
+++ website/templates/service-worker.js
@@ -1,10 +1,76 @@
 if ('serviceWorker' in navigator) {
-    console.log("Will the service worker register?");
-    navigator.serviceWorker.register('service-worker.js', {scope: '.'})
-        .then(function (reg) {
-            self.addEventListener('fetch', function (e) {});
-            console.log('ServiceWorker registration successful with scope: ', reg.scope);
-        }).catch(function (err) {
-        console.log("No it didn't. This happened: ", err)
-    });
+  navigator.serviceWorker.register('service-worker.js', {scope: '.'});
 }
+
+// Names of the two caches used in this version of the service worker.
+// Change to v2, etc. when you update any of the local resources, which will
+// in turn trigger the install event again.
+const PRECACHE = 'precache-v1';
+const RUNTIME = 'runtime';
+
+// A list of local resources we always want to be cached.
+const PRECACHE_URLS = [
+    './', // Alias for index.html
+    '/about',
+    'static/css/style.css',
+    'static/css/catalog.css',
+    'static/css/pure/pure-min.css',
+    'static/css/grids-min.css',
+    'static/css/grids-responsive-min.css',
+    'static/css/mapbox-gl.css',
+    'static/js/mapbox-gl.js',
+    'static/js/station_details.js',
+    'static/js/moment.min.js',
+    'static/js/Chart.min.js',
+    '/static/image/logo_md.png',
+    'https://fonts.googleapis.com/css?family=Open+Sans:300',
+    'https://use.fontawesome.com/releases/v5.0.13/css/all.css'
+];
+
+// The install handler takes care of precaching the resources we always need.
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(PRECACHE)
+            .then(cache => cache.addAll(PRECACHE_URLS))
+            .then(self.skipWaiting())
+    );
+});
+
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener('activate', event => {
+    const currentCaches = [PRECACHE, RUNTIME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+        }).then(cachesToDelete => {
+            return Promise.all(cachesToDelete.map(cacheToDelete => {
+                return caches.delete(cacheToDelete);
+            }));
+        }).then(() => self.clients.claim())
+    );
+});
+
+// The fetch handler serves responses for same-origin resources from a cache.
+// If no response is found, it populates the runtime cache with the response
+// from the network before returning it to the page.
+self.addEventListener('fetch', event => {
+    // Skip cross-origin requests, like those for Google Analytics.
+    if (event.request.url.startsWith(self.location.origin)) {
+        event.respondWith(
+            caches.match(event.request).then(cachedResponse => {
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
+
+                return caches.open(RUNTIME).then(cache => {
+                    return fetch(event.request).then(response => {
+                        // Put a copy of the response in the runtime cache.
+                        return cache.put(event.request, response.clone()).then(() => {
+                            return response;
+                        });
+                    });
+                });
+            })
+        );
+    }
+});
